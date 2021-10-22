0bf09e3833e23c5078d18f63c32e33938464e287
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-10-22 19:58:59 +0200

    config(init): add env configs

---
+++ src/config/config.js
@@ -0,0 +1,82 @@
+import dotenv from 'dotenv';
+import Joi from 'joi';
+
+dotenv.config({ path: '.env' });
+
+const envVarsSchema = Joi.object()
+  .keys({
+    NODE_ENV: Joi.string()
+      .valid('production', 'development', 'test')
+      .required(),
+    PORT: Joi.number().default(3000),
+    DATABASE_CONNECTION: Joi.string().required(),
+    DATABASE_PASSWORD: Joi.string().required(),
+    AWS_ACCESS_KEY_ID: Joi.string().required(),
+    AWS_SECRET_ACCESS_KEY: Joi.string().required(),
+    AWS_REGION: Joi.string().required(),
+    AWS_BUCKET_NAME: Joi.string().required(),
+    JWT_SECRET: Joi.string().required(),
+    JWT_ACCESS_EXPIRATION_MINUTES: Joi.number().required(),
+    JWT_REFRESH_EXPIRATION_DAYS: Joi.number().required(),
+    JWT_RESET_PASSWORD_EXPIRATION_MINUTES: Joi.number().required(),
+    JWT_VERIFY_EMAIL_EXPIRATION_MINUTES: Joi.number().required(),
+    SMTP_HOST: Joi.string().required(),
+    SMTP_PORT: Joi.number().required(),
+    SMTP_USERNAME: Joi.string().required(),
+    SMTP_PASSWORD: Joi.string().required(),
+    EMAIL_FROM: Joi.string().required()
+  })
+  .unknown();
+
+const { value: envVars, error } = envVarsSchema
+  .prefs({ errors: { label: 'key' } })
+  .validate(process.env);
+
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`);
+}
+
+const config = {
+  env: envVars.NODE_ENV,
+  port: envVars.PORT,
+  db: {
+    url: envVars.DATABASE_CONNECTION,
+    pass: envVars.DATABASE_PASSWORD
+  },
+  aws: {
+    accessKey: {
+      id: envVars.AWS_ACCESS_KEY_ID,
+      secret: envVars.AWS_SECRET_ACCESS_KEY
+    },
+    region: envVars.AWS_REGION,
+    bucketName: envVars.AWS_BUCKET_NAME
+  },
+  jwt: {
+    secret: envVars.JWT_SECRET,
+    accessToken: {
+      expire: envVars.JWT_ACCESS_EXPIRATION_MINUTES
+    },
+    refreshToken: {
+      expire: envVars.JWT_REFRESH_EXPIRATION_DAYS
+    },
+    resetPasswordToken: {
+      expire: envVars.JWT_RESET_PASSWORD_EXPIRATION_MINUTES
+    },
+    verifyEmailToken: {
+      expire: envVars.JWT_VERIFY_EMAIL_EXPIRATION_MINUTES
+    }
+  },
+  email: {
+    smtp: {
+      host: envVars.SMTP_HOST,
+      port: envVars.SMTP_PORT,
+      auth: {
+        user: envVars.SMTP_USERNAME,
+        pass: envVars.SMTP_PASSWORD
+      }
+    },
+    from: envVars.EMAIL_FROM
+  }
+};
+
+export default config;
