4eba863b4eb6cf18c20de44d17071dc2fb1b1c4f
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-10-24 01:52:06 +0200

    feat(model): add model index file

---
+++ src/models/index.js
@@ -0,0 +1,21 @@
+import User from './user.model';
+import Token from './token.model';
+import Video from './video.model';
+import Category from './category.model';
+import Feeling from './feeling.model';
+import History from './history.model';
+import Subscription from './subscription.model';
+import Comment from './comment.model';
+import Reply from './reply.model';
+
+export {
+  User,
+  Token,
+  Video,
+  Category,
+  Feeling,
+  History,
+  Subscription,
+  Comment,
+  Reply
+};
