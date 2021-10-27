9a0eae61147d33862785fd40cecce26dd7e2b645
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-10-27 12:59:13 +0200

    feat(index): add category and video routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,17 @@
 import express from 'express';
 
 import authRoutes from './auth.route';
-import userRoutes from './user.route';
 import profileRoutes from './profile.route';
+import userRoutes from './user.route';
+import categoryRoutes from './category.route';
+import videoRoutes from './video.route';
 
 const router = express.Router();
 
 router.use('/auth', authRoutes);
-router.use('/users', userRoutes);
 router.use('/profile', profileRoutes);
+router.use('/users', userRoutes);
+router.use('/categories', categoryRoutes);
+router.use('/videos', videoRoutes);
 
 export default router;
