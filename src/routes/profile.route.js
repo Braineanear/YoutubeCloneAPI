5edef1b2daf7c5a9d496cca539814dbdca54deba
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-10-27 12:57:39 +0200

    fix(route): fix multer bug in the profile image update route

--- src/routes/profile.route.js
+++ src/routes/profile.route.js
@@ -1,11 +1,10 @@
 import express from 'express';
-import multer from 'multer';
+
+import { singleImage } from '../utils/multer';
 
 import protect from '../middlewares/protect';
 
 import { profileController } from '../controllers/index';
-
-const upload = multer({ dest: 'uploads/' });
 
 const {
   getMyData,
@@ -21,6 +20,6 @@ router.use(protect);
 
 router.route('/').get(getMyData).patch(updateMyData).delete(deleteMyAccount);
 router.route('/change-password').patch(changeMyPassword);
-router.route('/image').patch(upload.single('image'), updateMyProfileImage);
+router.route('/image').patch(singleImage('image'), updateMyProfileImage);
 
 export default router;
