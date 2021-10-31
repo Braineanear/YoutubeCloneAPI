37429609ef21beaddeb8430ad56a249c49e84cfd
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-10-31 17:24:51 +0200

    feat(controller): add redis get/set to controller

--- src/controllers/category.controller.js
+++ src/controllers/category.controller.js
@@ -1,6 +1,7 @@
 import AppError from '../utils/appError';
 import catchAsync from '../utils/catchAsync';
 import APIFeatures from '../utils/apiFeatures';
+import { generateCacheKey, setValue, getValue } from '../utils/redis';
 
 import { Category } from '../models/index';
 
@@ -10,11 +11,37 @@ import { Category } from '../models/index';
  * @access    Public
  */
 export const getAllCategories = catchAsync(async (req, res, next) => {
+  let { page, sort, limit, select } = req.query;
+
+  if (!page) page = 1;
+  if (!sort) sort = '';
+  if (!limit) limit = 10;
+  if (!select) select = '';
+
+  const key = generateCacheKey(
+    'categories',
+    `page:${page}-sortBy:${sort}-limit:${limit}-select:${select}`
+  );
+
+  let cached = await getValue(key);
+
+  cached = JSON.parse(cached);
+
+  if (cached) {
+    return res.status(200).json({
+      status: 'success',
+      message: req.polyglot.t('successfulCategoriesFound'),
+      categories: cached
+    });
+  }
+
   const categories = await APIFeatures(req, Category);
 
   if (!categories) {
     return next(new AppError(req.polyglot.t('noCategoriesFound'), 404));
   }
+
+  setValue(key, JSON.stringify(categories), 60);
 
   return res.status(200).json({
     status: 'success',
@@ -31,11 +58,27 @@ export const getAllCategories = catchAsync(async (req, res, next) => {
 export const getCategory = catchAsync(async (req, res, next) => {
   const { id: categoryId } = req.params;
 
+  const key = generateCacheKey('category', categoryId);
+
+  let cached = await getValue(key);
+
+  cached = JSON.parse(cached);
+
+  if (cached) {
+    return res.status(200).json({
+      status: 'success',
+      message: req.polyglot.t('successfulCategoryFound'),
+      category: cached
+    });
+  }
+
   const category = await Category.findById(categoryId);
 
   if (!category) {
     return next(new AppError(req.polyglot.t('noCategoryFound'), 404));
   }
+
+  setValue(key, JSON.stringify(category), 60);
 
   return res.status(200).json({
     status: 'success',
