6d387d87649cc9c9909db067a2a651896445b7d8
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-11-06 14:23:11 +0200

    update(redis): add host, port and password configs to redis client

--- src/config/redis.js
+++ src/config/redis.js
@@ -3,21 +3,25 @@ import redis from 'redis';
 import config from './config';
 import logger from './logger';
 
-const redisPort = config.redis.port;
+const { host, port, password } = config.redis;
 
 /**
  * Create Redis Client
  * @param {number} redisPort
  * @returns {Object}
  */
-const client = redis.createClient(redisPort);
+const client = redis.createClient({ host, port, password });
+
+client.on('ready', () => {
+  logger.info('Redis client ready');
+});
 
 client.on('connect', () => {
   logger.info('Redis client connected');
 });
 
 client.on('error', (err) => {
-  logger.error(`Redis Error: ${err}`);
+  logger.error(`Redis client error: ${err}`);
 });
 
 export default client;
